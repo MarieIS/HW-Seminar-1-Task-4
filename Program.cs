7b13937dc7d3527adbe9ac88da56a50f005ece8f
Maria <mariya-ivanova@list.ru>
2023-12-28 20:05:19 +0200

    New console was added

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Insert number1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Insert number2: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Insert number3: ");
+int number3 = int.Parse(Console.ReadLine());
+
+ if (number1 == number2 || number1 == number3 || number2 == number3)
+ {
+    Console.WriteLine("no maximum");
+ }
+
+ else if (number1 > number2 && number1 > number3)
+ {
+    Console.WriteLine($"{number1} is the maximum");
+ }
+
+ else if (number2 > number1 && number2 > number3)
+ {
+    Console.WriteLine($"{number2} is the maximum");
+ }
+
+ else
+ {
+    Console.WriteLine($"{number3} is the maximum");
+ }
